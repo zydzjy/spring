f2f874d259df3016decd0b4d964c0539352e29af
zydzjy <914778290@qq.com>
2020-09-24 20:44:24 +0800

    none

---
+++ springWebService/src/main/java/springWebService/ws/GetStudentDetailsRequest.java
@@ -0,0 +1,61 @@
+//
+// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.3.2 生成的
+// 请访问 <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// 在重新编译源模式时, 对此文件的所有修改都将丢失。
+// 生成时间: 2020.09.24 时间 08:32:48 PM CST 
+//
+
+
+package springWebService.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>anonymous complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "id"
+})
+@XmlRootElement(name = "GetStudentDetailsRequest")
+public class GetStudentDetailsRequest {
+
+    protected int id;
+
+    /**
+     * 获取id属性的值。
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置id属性的值。
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+}
